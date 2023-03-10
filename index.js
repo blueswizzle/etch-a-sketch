26447054b36f0856aeb895908f849d1ecebe65ef
Atheek Ashkar <atheek99@yahoo.com>
2023-03-10 13:23:17 -0500

    Added some control options and an input slider that will control the size of the grid.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const grid = document.getElementById("grid-container");
-
+let mouseDown = false;
+let gridSlider = document.getElementById("grid-size");
+let size = 8;
+document.body.addEventListener("mousedown", () => {
+    mouseDown = true;
+    console.log(mouseDown);
+});
+document.body.addEventListener("mouseup", () =>{
+    mouseDown = false;
+    console.log(mouseDown);
+})
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -12,7 +22,11 @@ for (let i = 0; i < 16; i++) {
     }
     
     grid.appendChild(row);
-  }
-    
+}
+
+gridSlider.oninput = () =>{
+    size = gridSlider.value;
+    document.getElementById("size-label").innerHTML = `Grid Size: ${size} x ${size}`;
+}
   
   
